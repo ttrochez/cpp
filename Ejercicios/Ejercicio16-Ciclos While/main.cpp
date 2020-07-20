1788ce62357a3c92bd5bb8203299a0c42a64f20c
Thower Trochez <ttrochezescalante@gmail.com>
2020-07-19 22:50:38 -0600

    Agregando Ciclos While

--- Ejercicios/Ejercicio16-Ciclos While/main.cpp
+++ Ejercicios/Ejercicio16-Ciclos While/main.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 using namespace std;
-
 
 int main(int argc, char** argv) {
 	
 	int a = 10;
 	int b = 3;
-		
+	
 	while(a > b){
-		cout <<"Ingrese el valor de a: " <<endl;
+		cout <<"Ingrese el valor de a:" <<endl;
 		cin >> a;
+			
 	}
+			
+	cout <<endl;
+	cout <<"Saliste del ciclo" ;
+	 
 	
-	cout <<"Saliste del ciclo";
-	
-
 
 	return 0;
 }
