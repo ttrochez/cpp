bf4d595b850650ad53767daa37deb218b0b1ddec
Thower Trochez <ttrochezescalante@gmail.com>
2020-07-24 00:36:37 -0600

    agregando variantes del ciclo for

---
+++ Ejercicios/Ejercicio23-Variantes del Ciclo For/ciclofor.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 0; i < 15; i++)
+    {
+        cout << i << " ";
+    }
+
+    cout <<endl;
+    cout <<endl;
+
+    for (int i = 14; i >= 0 ; i--)
+    {
+        cout << i << " ";
+    }
+    
+    cout <<endl;
+    cout <<endl;
+
+    for (int i = 1000; i < 1000000; i++)
+    {
+        cout << i <<" ";
+        if (i == 1050)
+        {
+            break;
+        }
+        
+    }
+    
+    cout <<endl;
+    cout <<endl;
+
+    for (int i = 0; i < 15; i++)
+    {
+       
+        if (i == 5 || i == 10)
+        {
+            continue;
+        }
+
+        cout << i <<" "; 
+    }
+
+
+
+
+
+
+
+
+
+    return 0;
+}
