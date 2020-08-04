e517b5403b1cdd7446f3044e8bda4214097dbd63
Thower Trochez <ttrochezescalante@gmail.com>
2020-08-03 23:33:01 -0600

    agregando repaso 1

---
+++ Ejercicios/Ejercicio26-Repaso-1/repaso1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout <<"Indice: " << i <<endl;
+    }
+    
+    cout <<endl;
+    cout <<endl;
+
+    int i = 1;
+    while (i <=10)
+    {
+        cout <<"Indice: " <<i <<endl;
+        i++;
+    }
+    
+    
+    return 0;
+}
