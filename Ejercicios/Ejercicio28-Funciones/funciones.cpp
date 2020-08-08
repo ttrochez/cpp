9e8ec01b10ce452deebd977cc20fc6c56b8cae74
Thower Trochez <ttrochezescalante@gmail.com>
2020-08-08 01:30:32 -0600

    agregando funciones

---
+++ Ejercicios/Ejercicio28-Funciones/funciones.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int sumar(){
+    return 5 + 7;
+}
+
+string nombreCompleto(){
+    string nombre = "Juan";
+    string apellido = "Perez";
+    
+    return nombre + " " + apellido;
+}
+
+int main(int argc, char const *argv[])
+{
+    int resultado = sumar();
+    string nombreyApellido = nombreCompleto();
+    
+    cout << resultado <<endl;
+
+    cout <<nombreyApellido;
+
+    return 0;
+}
