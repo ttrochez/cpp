063b873f3fea5114f34a2de33096ccd652cbee95
Thower Trochez <ttrochezescalante@gmail.com>
2020-08-08 13:09:38 -0600

    agregando funcion int

---
+++ Ejercicios/Ejercicio29-Funciones-int/funcion-int.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int sumar(int a, int b) {
+    return a + b;
+} 
+
+int main(int argc, char const *argv[])
+{
+    system("cls");
+
+    cout << sumar(5,7);
+    cout <<endl;
+    cout << sumar(15,27);
+    cout <<endl;
+    cout << sumar(5,67);
+    cout <<endl;
+    cout << sumar(25,17);
+    cout <<endl;
+    
+
+    return 0;
+}
