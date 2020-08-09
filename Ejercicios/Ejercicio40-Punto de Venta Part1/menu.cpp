0b97d85d3bd94fcde921b44a774020d4f38e197f
Thower Trochez <ttrochezescalante@gmail.com>
2020-08-09 12:52:53 -0600

    agregando ejercicio punto de venta part1

---
+++ Ejercicios/Ejercicio40-Punto de Venta Part1/menu.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+void menu()
+{
+    int opcion = 0 ;
+
+    while(true)
+    {
+        system("cls");
+        
+        cout <<"****" <<endl;
+        cout <<"MENU" <<endl;
+        cout <<"****" <<endl;
+        cout <<endl;
+
+        cout <<"1 - Bebidas calientes" <<endl;
+        cout <<"2 - Bebidas Frias" <<endl;
+        cout <<"3 - Reposteria" <<endl;
+        cout <<"0 - Salir" <<endl;
+
+        cout <<"Ingrese una opcion entre 1 - 3: ";
+
+        cin >> opcion;
+
+        if (opcion == 0)
+        {
+            cout <<"Saliste del Menu" ;
+            break;
+        }
+        
+    }
+
+
+
+}
