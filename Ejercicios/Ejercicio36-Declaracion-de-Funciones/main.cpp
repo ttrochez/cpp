08767e3f53ad4858963dc5478128767af2a53559
Thower Trochez <ttrochezescalante@gmail.com>
2020-08-09 01:32:43 -0600

    ejercicio alcance de las variables

---
+++ Ejercicios/Ejercicio36-Declaracion-de-Funciones/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int numero = 0;
+int sumar(int a, int b);
+
+int main(int argc, char const *argv[])
+{
+    numero = 5;
+    cout << sumar(5, 7) <<endl;
+
+    cout << numero;
+    
+    return 0;
+}
+
+int sumar(int a, int b)
+{
+    numero = 7;
+    return a + b;
+}
+
