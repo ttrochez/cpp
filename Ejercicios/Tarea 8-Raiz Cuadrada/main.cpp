0fe81596829b349a296bc21d2deafe21cc88f46a
Thower Trochez <ttrochezescalante@gmail.com>
2020-08-10 15:09:13 -0600

    agregando tarea 8

---
+++ Ejercicios/Tarea 8-Raiz Cuadrada/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    float num = 0;
+    float resultado = 0;
+
+    cout << "Ingrese el numero a calcular: ";
+    cin >> num;
+
+    resultado = (sqrt (num));
+
+    cout <<endl <<endl;
+
+    cout <<"La raiz cuadrada de: exit
+    " << num <<" es: " << resultado;
+    
+
+    return 0;
+}
